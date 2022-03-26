1ea6a3794f815c7adc58449613b23b5df136ff98
buraknejats <nejat.980@hotmail.com>
2022-03-26 22:52:29 +0300

    Todo List App
    In this project, you can manage your to-do list. This project was created using react.

---
+++ to-do-list-1/src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos"; 
+
+function App() {
+ const [todos, setTodos] = useState([]);
+   
+ useEffect(() => {
+const storageTados = JSON.parse ( localStorage.getItem(LOCAL_STORAGE_KEY)); 
+if (storageTados) {
+  setTodos(storageTados);
+}
+ }, [] );
+
+ useEffect(() => {
+   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify( todos));
+
+ }, [todos] );
+
+function addTodo(todo){
+setTodos([todo, ...todos]);
+ 
+function toggleComplete(id) {
+setTodos(
+todos.map(todo => {
+  if(todo.id === id ) {
+return {
+...todo,
+completed: !todo.completed
+};
+
+  }
+  return todo;
+})
+);
+
+}
+
+function removeTodo (id) {
+setTodos(todos.filter(todo => todo.id !== id ));
+}
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>React Todo</p>
+       <TodoForm addTodo={addTodo} />
+       <TodoList todos={todos}  toggleComplete={toggleComplete}
+       removeTodo={removeTodo}
+       />
+      </header>
+    </div>
+  );
+} 
+}
+
+export default App; 
---
+++ to-do-list-1/src/components/Todo.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function Todo({todo, toggleComplete , removeTodo}) {
+    function handleCheckboxClick(){
+        toggleComplete(todo.id);
+    }
+
+    function handleRemoveClick() {
+        removeTodo(todo.id)
+    }
+    return(
+        <div style={{display:"flex"}}>
+   <input type="checkbox" onClick={handleCheckboxClick} />
+   <li
+style={{
+    color: "white",
+    textDecoration: todo.completed ? "Line-Through" : null
+}}
+   >
+
+       
+          {todo.task}
+        </li>
+       <button onClick={handleRemoveClick}> X </button>
+   </div>
+    );
+
+}
+
+
+export default Todo;
